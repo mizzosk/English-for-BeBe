4e6ebbe8df994f1c18fb612e21fa5a5628ee0807
Michal Bollardt <Bollardt@sk.ibm.com>
2024-01-19 23:20:45 +0100

    random number generation update
    with this change word that was already correctly answered ss removed from the vocabListNewGame list so the game goes until all the words are answered only

--- index.js
+++ index.js
@@ -38,46 +38,79 @@
 // )
 
 
-
+// vocabulary list for new words
 var vocabListNewEn = ["living room","dinning room", "kitchen", "bathroom"];
 var vocabListNewSk = ["obývacia izba","jedáleň", "kuchyňa", "kúpeľňa"];
+var vocabListNewGameEn= [];
+var vocabListNewGameSk = [];
+var newWord = document.getElementById("newWordId");
+var counter =  0;
 
-// vocabListNew random word generation and comarison with alert if the list do no have the same number of words
-var vocabListNewRandomWord = Math.floor(Math.random() * vocabListNewEn.length);
+// filling vocabListNewGamewith 2x of the original words
+for (var i = 0; i < vocabListNewEn.length; i++) {
+  for (var j = 0; j < 2; j++) {
+    vocabListNewGameEn.push(vocabListNewEn[i]);
+    vocabListNewGameSk.push(vocabListNewSk[i]);
+  }
+}
+// chech whether vocabListNewEn and vocabListNewSk have the same number of words
 if (vocabListNewEn.length!==vocabListNewSk.length){
     alert("Slovak and English vocabulary lists do not have the same number of words!");
 }
 
-// randomly switch between En and Sk list upon refresh
-var randomSwitch= Math.floor(Math.random() * 2);
-if (randomSwitch===0){
-    var vocabListNew = vocabListNewEn;
+function randomNewWordGeneration(){
+// randomly switch between En and Sk list
+if (vocabListNewGameEn.length === 0){
+    var randomSwitch = 1;
+} else if (vocabListNewGameSk.length === 0){
+    var randomSwitch = 0;
 } else {
-    var vocabListNew = vocabListNewSk;
+var randomSwitch = Math.floor(Math.random() * 2);
 }
 
-// generate random new word upon page refresh
-document.addEventListener("DOMContentLoaded", function() {
-    var newWord = document.getElementById("newWordId");
-    newWord.innerHTML = vocabListNew[vocabListNewRandomWord];
-  });
-
-
-// translate word  
-document.getElementById("newWordId").addEventListener("click",function (){
-        
-    if (randomSwitch===0){
-        randomSwitch=1;
-        vocabListNew = vocabListNewSk;
-    } else {
-        randomSwitch=0;
-        vocabListNew = vocabListNewEn;
+if(vocabListNewGameEn.length === 0 && vocabListNewGameSk.length === 0){
+    alert("Game OVER!!");
+}else{
+    if (randomSwitch===0 ){
+        var vocabListNew = vocabListNewGameEn;
+        var vocabListNewRandomWord = Math.floor(Math.random() * vocabListNewGameEn.length);
+    } if (randomSwitch===1){
+        var vocabListNew = vocabListNewGameSk;
+        var vocabListNewRandomWord = Math.floor(Math.random() * vocabListNewGameSk.length);
     }
-    this.innerHTML = vocabListNew[vocabListNewRandomWord];
-        });
+    // displaying random word from en or sk vocabulary list
+    newWord.innerHTML = vocabListNew[vocabListNewRandomWord];
+    counter++;
+    console.log(counter);
+
+    if (vocabListNewGameEn.includes(newWord.innerHTML)){
+        var index = vocabListNewGameEn.indexOf(newWord.innerHTML);
+        vocabListNewGameEn.splice(index,1);
+    } 
+    if (vocabListNewGameSk.includes(newWord.innerHTML)){
+        var index = vocabListNewGameSk.indexOf(newWord.innerHTML);
+        vocabListNewGameSk.splice(index,1);
+    }
+    console.log(vocabListNewGameEn);
+    console.log(vocabListNewGameSk);
+    }
+}
+// generate the random word upon page load
+randomNewWordGeneration()
+
+// translate word upon click on the word  
+document.getElementById("newWordId").addEventListener("click",function (){
+    if (vocabListNewEn.includes(this.innerHTML)){
+        var index = vocabListNewEn.indexOf(this.innerHTML);
+        newWord.innerHTML = vocabListNewSk[index];
+   } else {
+    var index = vocabListNewSk.indexOf(this.innerHTML);
+    newWord.innerHTML = vocabListNewEn[index];
+   }
+   
+});
   
 // refresh button        
 document.getElementById("refresh").addEventListener("click",function (){
-    
-location.reload();
+    randomNewWordGeneration()
 });
